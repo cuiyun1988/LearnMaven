3188375c698a8064e1d56f16776151663615fbc9
Administrator <1737401666@qq.com>
2019-06-13 13:03:07 +0800

    commit mavendao

---
+++ mavenDao/src/main/java/com/cy/mavenDao/UserDaoI.java
@@ -0,0 +1,8 @@
+package com.cy.mavenDao;
+
+import mavenModel.User;
+
+public interface UserDaoI {
+
+	public void save(User user);
+}
---
+++ mavenDao/src/main/java/com/cy/mavenDaoImpl/UserDaoImpl.java
@@ -0,0 +1,14 @@
+package com.cy.mavenDaoImpl;
+
+import com.cy.mavenDao.UserDaoI;
+
+import mavenModel.User;
+
+public class UserDaoImpl implements UserDaoI {
+
+
+	public void save(User user) {
+		  System.out.println("±£´æ" );		
+	}
+
+}
